5473aa03c5a859afa3618e286eac284adf3fdd90
El-hassanRamadan-debug <hassan1932005@gmail.com>
2025-11-30 20:46:36 +0200

    we have

--- src/SudokuBoard.java
+++ src/SudokuBoard.java
@@ -4,7 +4,7 @@ public class SudokuBoard {
     private final int[][] grid;
 
     public SudokuBoard(String path) throws IOException {
-        this.grid = CSVReader.readBoard(path); // Your CSV reader code
+        this.grid = CSVReader.readBoard(path);
     }
 
     public int[][] getGrid() {
