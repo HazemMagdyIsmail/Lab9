a3d8ea08ab712a2a2cf3abf31c4a59947cf41088
El-hassanRamadan-debug <hassan1932005@gmail.com>
2025-11-30 20:41:34 +0200

    done

--- src/DuplicateInfo.java
+++ src/DuplicateInfo.java
@@ -1,8 +1,8 @@
 public class DuplicateInfo {
     private final RegionType regionType;
     private final int index;      // 1-based index of region
-    private final int value;      // duplicated value
-    private final int[] positions; // 1-based positions of duplicates in region
+    private final int value; 
+    private final int[] positions; 
 
     public DuplicateInfo(RegionType regionType, int index, int value, int[] positions) {
         this.regionType = regionType;
